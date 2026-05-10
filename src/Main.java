3f11a98d940b105ebb2f8e7610ca9e8627ba3444
Kortunov Yura <kortunov.yura@mail.ru>
2026-05-10 16:43:00 +0500

    дз

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        printThreeWords();
+    }
+
+    public static void printThreeWords() {
+        System.out.print("Orange \nBanana \nApple");
+    }
+}
