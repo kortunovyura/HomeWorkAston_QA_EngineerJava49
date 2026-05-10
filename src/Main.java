f35f6c6cb62a8e7507c7420ae8d7ee9328d6193f
Kortunov Yura <kortunov.yura@mail.ru>
2026-05-10 17:33:30 +0500

    дз

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        printThreeWords();
+        checkSumSign();
     }
 
     public static void printThreeWords() {
         System.out.print("Orange \nBanana \nApple");
     }
+
+    public static void checkSumSign() {
+        int a = 10;
+        int b = -5;
+        int sum = a + b;
+        if (sum >= 0) {
+            System.out.print("Сумма положительная");
+        } else {
+            System.out.print("Сумма отрицательная");
+        }
+    }
+
 }
