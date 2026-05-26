ce7df9237e3e2ff6842ca90e8253296b3de32761
Yura <kortunov.yura@mail.ru>
2026-05-26 10:48:17 +0500

    Update FactorialCalculator.java

--- src/java/Lesson_7_junit_5/FactorialCalculator.java
+++ src/java/Lesson_7_junit_5/FactorialCalculator.java
@@ -1,1 +1,12 @@
-public class FactorialCalculator
+public class FactorialCalculator{
+      public long calculate(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Число должно быть больше или равно 0");
+        }
+        long result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
