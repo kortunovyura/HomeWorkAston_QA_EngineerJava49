97161440d8b325c865354f56353fd9169b3a9fba
Yura <kortunov.yura@mail.ru>
2026-05-30 19:52:18 +0500

    Update NumberComparatorTestJunit.java

--- src/java/Lesson_7_junit_5/NumberComparatorTestJunit.java
+++ src/java/Lesson_7_junit_5/NumberComparatorTestJunit.java
@@ -7,17 +7,17 @@ class NumberComparatorTestJunit {
     private final NumberComparator comparator = new NumberComparator();
 
     @Test
-    void testFirstNumberIsGreater() {
+    public void testFirstNumberIsGreater() {
         assertEquals(1, comparator.compare(10, 5));
     }
 
     @Test
-    void testSecondNumberIsGreater() {
+    public void testSecondNumberIsGreater() {
         assertEquals(-1, comparator.compare(3, 7));
     }
 
     @Test
-    void testNumbersAreEqual() {
+    public void testNumbersAreEqual() {
         assertEquals(0, comparator.compare(5, 5));
     }
 }
