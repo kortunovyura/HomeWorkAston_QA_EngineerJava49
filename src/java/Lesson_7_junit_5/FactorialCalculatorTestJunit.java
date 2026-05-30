f052bab585adeaa79e8df68d257fd205d6df3e49
Yura <kortunov.yura@mail.ru>
2026-05-30 19:51:38 +0500

    Update FactorialCalculatorTestJunit.java

--- src/java/Lesson_7_junit_5/FactorialCalculatorTestJunit.java
+++ src/java/Lesson_7_junit_5/FactorialCalculatorTestJunit.java
@@ -6,17 +6,17 @@ class FactorialCalculatorTestJunit {
     private final FactorialCalculator calculator = new FactorialCalculator();
 
     @Test
-    void testFactorialOfZero() {
+    public void testFactorialOfZero() {
         assertEquals(1, calculator.calculate(0));
     }
 
     @Test
-    void testFactorialOfPositiveNumber() {
+    public void testFactorialOfPositiveNumber() {
         assertEquals(120, calculator.calculate(5));
     }
 
     @Test
-    void testFactorialExceptionForNegativeNumber() {
+    public void testFactorialExceptionForNegativeNumber() {
         assertThrows(IllegalArgumentException.class, () -> calculator.calculate(-1));
     }
 }
