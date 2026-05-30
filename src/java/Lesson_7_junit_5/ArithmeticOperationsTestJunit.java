d1398866690039f92981bc1057794abe70db82f8
Yura <kortunov.yura@mail.ru>
2026-05-30 19:50:24 +0500

    Update ArithmeticOperationsTestJunit.java

--- src/java/Lesson_7_junit_5/ArithmeticOperationsTestJunit.java
+++ src/java/Lesson_7_junit_5/ArithmeticOperationsTestJunit.java
@@ -6,27 +6,27 @@ class ArithmeticOperationsTestJunit {
     private final ArithmeticOperations math = new ArithmeticOperations();
 
     @Test
-    void testAddition() {
+    public void testAddition() {
         assertEquals(5, math.add(2, 3));
     }
 
     @Test
-    void testSubtraction() {
+    public void testSubtraction() {
         assertEquals(2, math.subtract(5, 3));
     }
 
     @Test
-    void testMultiplication() {
+    public void testMultiplication() {
         assertEquals(15, math.multiply(3, 5));
     }
 
     @Test
-    void testDivision() {
+    public void testDivision() {
         assertEquals(2.5, math.divide(5, 2));
     }
 
     @Test
-    void testDivisionByZero() {
+    public void testDivisionByZero() {
         assertThrows(ArithmeticException.class, () -> math.divide(5, 0));
     }
 }
