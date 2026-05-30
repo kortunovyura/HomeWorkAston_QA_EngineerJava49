fe046990caf73851cb934a57ac95f05436dfb53a
Yura <kortunov.yura@mail.ru>
2026-05-30 19:56:44 +0500

    Update ArithmeticOperations.java

--- src/java/Lesson_7_junit_5/ArithmeticOperations.java
+++ src/java/Lesson_7_junit_5/ArithmeticOperations.java
@@ -1,7 +1,17 @@
 public class ArithmeticOperations {
-    public int add(int a, int b) { return a + b; }
-    public int subtract(int a, int b) { return a - b; }
-    public int multiply(int a, int b) { return a * b; }
+    
+    public int add(int a, int b) { 
+        return a + b; 
+    }
+    
+    public int subtract(int a, int b) { 
+        return a - b; 
+    }
+    
+    public int multiply(int a, int b) { 
+        return a * b; 
+    }
+    
     public double divide(int a, int b) {
         if (b == 0) {
             throw new ArithmeticException("Деление на ноль запрещено");
