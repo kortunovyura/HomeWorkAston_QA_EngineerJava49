98e15567e0ab1a7157fbeb5bbd07ffa12462650e
Yura <kortunov.yura@mail.ru>
2026-06-15 14:47:36 +0500

    Update BaseTest.java

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,19 +1,50 @@
+package test;
+
 import driver.Driver;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import io.qameta.allure.Attachment;
+import lombok.AccessLevel;
+import lombok.Getter;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.junit.jupiter.api.extension.TestWatcher;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.Optional;
 
 public class BaseTest {
-    protected WebDriver driver;
+    @Getter(AccessLevel.PROTECTED)
+    private static WebDriver driver;
 
-    @BeforeEach
-    public void setUp() {
-        driver = Driver.initDriver();
-        driver.get("https://www.mts.by/");
+    @BeforeAll
+    public static void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        Driver.setOptions(options);
+        driver = Driver.getInstance();
     }
 
-    @AfterEach
-    public void tearDown() {
-        Driver.closeDriver();
+    @AfterAll
+    public static void tearDown() {
+        Driver.quit();
+    }
+
+    @RegisterExtension
+    TestWatcher watcher = new TestWatcher() {
+        @Override
+        public void testFailed(ExtensionContext context, Throwable cause) {
+            if (driver != null) {
+                saveScreenshot(context.getDisplayName() + "_failed");
+            }
+        }
+    };
+
+    @Attachment(value = "Скриншот падения {screenshotName}", type = "image/png")
+    public byte[] saveScreenshot(String screenshotName) {
+        return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
     }
 }
